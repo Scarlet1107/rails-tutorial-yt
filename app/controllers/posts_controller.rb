960ba78839adb7735b7afe4ab1bb85d8ff3992e8
Scarlet1107 <shougo.ushiro@gmail.com>
2025-02-16 17:40:33 +0900

    add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
   def index
+    # PostというテーブルのPostレコードを全て取得して、@postsに代入。これによって、ビューのindex.htmlで@postsを使えるようになる。
+    @posts = Post.all
+  end
+  def new
+    @posts = Post.new
   end
 end
