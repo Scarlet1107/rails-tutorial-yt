b75e87832b07a6f11df99f7b1bdc14778f4bfb24
Scarlet1107 <shougo.ushiro@gmail.com>
2025-03-21 16:54:15 +0900

    46:02

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   def new
-    @posts = Post.new
+    @post = Post.new
+  end
+  # 通常newとcreateはセットで使用される
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+          redirect_to posts_path
+    else
+       render :new
+    end
+  end
+
+  # post_paramsはpost controllerでしか呼び出せないようにする
+  private
+  def post_params
+        params.require(:post).permit(:title, :content)
   end
 end
